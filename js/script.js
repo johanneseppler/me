acce395bb8378ae4a7d31d85715a8ee94c96f8dc
pippowie <phil.pp@me.com>
2019-07-20 14:21:51 +0200

    added country emoji and updated content

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-// $(document).ready(function () {
-//   $("#pikaexe").click(pikarun());
-//  });
-
 function pikarun() {
   "use strict"
 
-  console.log("Is this working?");
+  console.log("Is this working?")
 
-  let data = 'https://i.imgur.com/g99m5gz.gif'
+  var data = 'https://i.imgur.com/g99m5gz.gif'
 
   var shock = document.createElement('div')
   var img = new Image()
@@ -24,62 +20,9 @@ function pikarun() {
 
   window.setTimeout(function () {
     img.style.left = 'calc(100% + 1000px)'
-  }, 50)
+  }, 100)
 
   window.setTimeout(function () {
     img.parentNode.removeChild(img)
-  }, 4300)
-
-
-  // let pikaRun = function () {
-  //
-  //   var shock = document.createElement('div')
-  //   var img = new Image()
-  //   img.src = data
-  //   img.style.width = '450px'
-  //   img.style.height = '350px'
-  //   img.style.transition = '4s all'
-  //   img.style.position = 'fixed'
-  //   img.style.left = '-400px'
-  //   img.style.bottom = '0px'
-  //   img.style.zIndex = 999999
-  //
-  //   document.body.appendChild(img)
-  //
-  //   // window.setTimeout(function(){
-  //   //   img.style.left = 'calc(50% - 200px)'
-  //   // },50)
-  //
-  //   window.setTimeout(function () {
-  //     img.style.left = 'calc(100% + 1000px)'
-  //   }, 50)
-  //
-  //   window.setTimeout(function () {
-  //     img.parentNode.removeChild(img)
-  //   }, 4300)
-  //
-  // }
-  //
-  // // let record = function (e) {
-  // //
-  // //   if (e.which === key[ck]) {
-  // //     ck++
-  // //   } else {
-  // //     ck = 0
-  // //   }
-  // //
-  // //   if (ck >= max) {
-  // //     pikaRun()
-  // //     ck = 0
-  // //   }
-  // //
-  // // }
-  //
-  // let init = function (data) {
-  //   $("#pika").addEventListener('click', pikaRun)
-  // }
-  //
-  // let data = 'https://i.imgur.com/g99m5gz.gif'
-  //
-  // init(data)
+  }, 5000)
 }
