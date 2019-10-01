15d6699a7f73e7011961b0ce84563a39818a13a5
pippowie <phil.pp@me.com>
2019-10-01 10:42:56 +0200

    minimized page content

--- js/script.js
+++ js/script.js
@@ -12,54 +12,6 @@ $('.toggler').click(function() {
 
 });
 
-// add ratings
-
-$(".stars").each(function() {
-  var i;
-  for (i = 0; i < $(this).attr('nr'); i++) {
-    $(this).append('<li class="list-inline-item mx-0"><i class="fas fa-star"></i></li>');
-  }
-  for (; i < 5; i++) {
-    $(this).append('<li class="list-inline-item mx-0"><i class="far fa-star"></i></li>');
-  }
-});
-
-// make pikachu run
-
-$('#pika').click(function() {
-  $(this).hide();
-  $('#pikaToggle').toggle(true);
-  pikarun();
-});
-
-function pikarun() {
-  "use strict"
-
-  var data = '../img/pikar.gif'
-
-  var shock = document.createElement('div')
-  var img = new Image()
-  img.src = data
-  img.style.width = '360px'
-  img.style.height = '280px'
-  img.style.transition = '4s all'
-  img.style.position = 'fixed'
-  img.style.left = '-400px'
-  img.style.bottom = '0px'
-  img.style.zIndex = 999999
-
-  document.body.appendChild(img)
-
-  window.setTimeout(function () {
-    img.style.left = 'calc(100% + 800px)'
-  }, 100)
-
-  window.setTimeout(function () {
-    img.parentNode.removeChild(img)
-  }, 5000)
-
-}
-
 // Particles effect
 
 window.onload = function() {
